28678e3e2095437f2901c08aab0104d352685102
bilal-alaabadi <mdbilalnizwa@gmail.com>
2025-05-26 13:52:55 +0400

    شرح للتعديل الذي قمت به

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
     cors({
-        origin:"https://www.henna-burgund.shop",
+        origin:"https://henna-f.vercel.app",
         // origin: "https://www.royasow.store",//مال الفرونت اند
         credentials: true,
     })
