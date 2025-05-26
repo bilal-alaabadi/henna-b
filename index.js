c78dd25669f98fee02a133d235cec43b31668c4c
bilal-alaabadi <mdbilalnizwa@gmail.com>
2025-05-27 00:00:47 +0400

    شرح للتعديل الذي قمت به

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(
 
 // دعم طلبات OPTIONS (Preflight Requests)
 app.options('*', (req, res) => {
-    res.header('Access-Control-Allow-Origin', 'https://henna-f.vercel.app');
+    res.header('Access-Control-Allow-Origin', 'https://www.henna-burgund.shop');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.send();
